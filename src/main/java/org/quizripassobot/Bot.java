ab6538cff1e0adf0ed7dbeb567c18a4525b16c9a
Samuele Max <samuele8max@gmail.com>
2023-10-13 13:00:26 +0200

    Inline keyboard buttons now working correctly

--- src/main/java/org/quizripassobot/Bot.java
+++ src/main/java/org/quizripassobot/Bot.java
@@ -2,10 +2,21 @@ package org.quizripassobot;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.AnswerCallbackQuery;
+import org.telegram.telegrambots.meta.api.methods.ParseMode;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.User;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Bot extends TelegramLongPollingBot {
     enum Status {
@@ -32,48 +43,67 @@ public class Bot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        var msg = update.getMessage();
-        var user = msg.getFrom();
-        var id = user.getId();
-        var msgText = msg.getText();
-
-        if (msg.isCommand()) {
-            switch (msgText) {
-                case "/stop":
+        if (update.hasMessage()) {
+            Message msg = update.getMessage();
+            User user = msg.getFrom();
+            long id = user.getId();
+            String msgText = msg.getText();
+            if (msg.isCommand()) {
+                switch (msgText) {
+                    case "/start":
+                        sendText(id, "Benvenuto! Cosa vuoi fare?");
+                        break;
+                    case "/stop":
+                        endQuiz(id);
+                        break;
+                    case "/newquiz":
+                        sendText(id, "Questa funzionalità non è ancora stata implementata");
+                        break;
+                    case "/loadquiz":
+                        status = Status.RECEIVING_QUIZ;
+                        sendText(id, "Carica un file di quiz compatibile");
+                        break;
+                    default:
+                        sendText(id, "Comando sconosciuto.");
+                }
+            } else {
+                switch (status) {
+                    case RECEIVING_QUIZ:
+                        status = Status.NORMAL;
+                        QuizQuestion[] quizQuestions = QuizQuestion.parseQuiz(msgText);
+                        if (quizQuestions == null) {
+                            sendText(id, "Il file che hai caricato non è nel formato corretto");
+                        } else {
+                            startQuiz(id, quizQuestions);
+                        }
+                        break;
+                    case RECEIVING_NEXT_ANSWER:
+                        answerReceived(id, msgText);
+                        break;
+                }
+            }
+        } else if (update.hasCallbackQuery()) {
+            var query = update.getCallbackQuery();
+            String data = query.getData();
+            long id = query.getFrom().getId();
+            switch (data) {
+                case "stop":
                     endQuiz(id);
                     break;
-                case "/newquiz":
-                    sendText(id, "Questa funzionalità non è ancora stata implementata");
-                    break;
-                case "/loadquiz":
+                case "loadquiz":
                     status = Status.RECEIVING_QUIZ;
                     sendText(id, "Carica un file di quiz compatibile");
-                    break;
-                default:
-                    sendText(id, "Comando sconosciuto.");
-            }
-        } else {
-            switch (status) {
-                case RECEIVING_QUIZ:
-                    status = Status.NORMAL;
-                    QuizQuestion[] quizQuestions = QuizQuestion.parseQuiz(msgText);
-                    if (quizQuestions == null) {
-                        sendText(id, "Il file che hai caricato non è nel formato corretto");
-                    } else {
-                        startQuiz(id, quizQuestions);
-                    }
-                    break;
-                case RECEIVING_NEXT_ANSWER:
-                    answerReceived(id, msgText);
                     break;
             }
         }
     }
 
     public void sendText(Long who, String what) {
+        var keyboard = getKeyboard();
         SendMessage sm = new SendMessage();
         sm.setChatId(who.toString());
         sm.setText(what);
+        sm.setReplyMarkup(keyboard);
         try {
             execute(sm);
         } catch (TelegramApiException e) {
@@ -122,6 +152,33 @@ public class Bot extends TelegramLongPollingBot {
         }
         sendText(userId, String.format("Il quiz è terminato. Hai risposto correttamente a %s domande su %s.", correctAnswers, quizLength));
         status = Status.NORMAL;
+    }
+
+    InlineKeyboardMarkup getKeyboard() {
+        var keyboard = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> keyboardRows = new ArrayList<>();
+        switch (status) {
+            case NORMAL:
+                var loadQuizButton = new InlineKeyboardButton();
+                loadQuizButton.setText("Load quiz");
+                loadQuizButton.setCallbackData("loadquiz");
+                keyboardRows.add(new ArrayList<>(List.of(loadQuizButton)));
+                break;
+            case RECEIVING_QUIZ:
+                var stopButton = new InlineKeyboardButton();
+                stopButton.setText("Stop");
+                stopButton.setCallbackData("stop");
+                keyboardRows.add(new ArrayList<>(List.of(stopButton)));
+                break;
+            case RECEIVING_NEXT_ANSWER:
+                stopButton = new InlineKeyboardButton();
+                stopButton.setText("Stop");
+                stopButton.setCallbackData("stop");
+                keyboardRows.add(new ArrayList<>(List.of(stopButton)));
+                break;
+        }
+        keyboard.setKeyboard(keyboardRows);
+        return keyboard;
     }
 
     public static void main(String[] args) throws TelegramApiException {
